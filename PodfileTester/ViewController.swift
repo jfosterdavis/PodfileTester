62479dcbd9eab44cb8d965fabd547ceba1fa60d1
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2018-01-22 17:38:27 -0500

    Initial Commit

---
+++ PodfileTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodfileTester
+//
+//  Created by Jacob Foster Davis on 1/22/18.
+//  Copyright © 2018 Zero Mu, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
